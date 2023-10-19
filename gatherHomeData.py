3dd1c1524333b76ad4463320822ca5bb76ae4f69
Thomas Keller <36502290+thomkell@users.noreply.github.com>
2023-10-19 15:14:37 -0700

    Update gatherHomeData.py

--- gatherHomeData.py
+++ gatherHomeData.py
@@ -20,20 +20,22 @@ with open('property_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
 
     for page_num in range(1, MAX_PAGES + 1):
         print(f"Scraping page {page_num}...")
-        URL = f"{BASE_URL}?ep={page_num}"
+        URL = f"{BASE_URL}?ep={page_num}&ipd=true"
 
         response = requests.get(URL, headers=HEADERS)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
 
-            # Extract locations based on the address tag
+            # Extract and process locations based on the address tag
             raw_locations = soup.find_all('address', translate="no")
             locations = []
             for loc in raw_locations:
                 match = re.search(r'\d{4}\s*(.*)', loc.text)
                 if match:
-                    locations.append(match.group(1).strip())
+                    location_name = match.group(1).strip()
+                    location_name = location_name.replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue')
+                    locations.append(location_name)
 
             # Extract prices
             prices = soup.find_all('span', class_='HgListingCard_price_sIIoV')
@@ -54,8 +56,20 @@ with open('property_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
 
                 # Extracting rooms and living area
                 detail_spans = details[i].find_all('span')
-                rooms = detail_spans[0].text.replace("Zimmer", "rooms").strip() if len(detail_spans) > 0 else 'N/A'
-                area = detail_spans[1].text.replace("Wohnfläche", "area").strip() if len(detail_spans) > 1 else 'N/A'
+                
+                if len(detail_spans) > 0:
+                    room_text = detail_spans[0].text.strip()
+                    match = re.search(r'(\d+\.?\d*)', room_text)  # Extract numeric value using regex
+                    rooms = match.group(1) if match else 'N/A'   # Store only the numeric value
+                else:
+                    rooms = 'N/A'
+
+                if len(detail_spans) > 1:
+                    area_text = detail_spans[1].text.strip()
+                    match = re.search(r'(\d+\.?\d*)', area_text)  # Extract numeric value using regex
+                    area = match.group(1) if match else 'N/A'    # Store only the numeric value
+                else:
+                    area = 'N/A'
 
                 # Write to CSV
                 writer.writerow({'Location': location, 'Price': cleaned_price, 'Rooms': rooms, 'Area': area})
